904a1b8c81d6341a45953c52b3933f7a9fc89d7c
Kishore GA <kishore7ga@gmil.com>
2026-08-09 13:05:28 +0530

    feat(landing): replace full landing page with 3D landing page from xite3d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,64 @@
-import { redirect } from "next/navigation";
+"use client";
 
-export const dynamic = "force-dynamic";
+import Section01WhatIsXite from "@/components/section1/Section01WhatIsXite";
+import Section02BuildItYourWay from "@/components/section2/Section02BuildItYourWay";
+import Section03EveryScreen from "@/components/section3/Section03EveryScreen";
+import Section04YourPages from "@/components/section4/Section04YourPages";
+import Section05DesignedFromSections from "@/components/section5/Section05DesignedFromSections";
+import Section06YourBrand from "@/components/section6/Section06YourBrand";
+import Section07YourControl from "@/components/section7/Section07YourControl";
+import Section08EveryInstitution from "@/components/section8/Section08EveryInstitution";
+import Section09MakeItYours from "@/components/section9/Section09MakeItYours";
+import Section10CompleteGovernance from "@/components/section10/Section10CompleteGovernance";
+import Section11EngineeredForScale from "@/components/section11/Section11EngineeredForScale";
+import Section12OneExperience from "@/components/section12/Section12OneExperience";
+import Section13BuildWhatComesNext from "@/components/section13/Section13BuildWhatComesNext";
 
-export default async function HomePage() {
-  redirect("/editor/mec");
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#000000] text-white">
+      {/* SECTION 01 */}
+      <Section01WhatIsXite />
+
+      {/* SECTION 02 */}
+      <Section02BuildItYourWay />
+
+      {/* SECTION 03 */}
+      <Section03EveryScreen />
+
+      {/* SECTION 04 */}
+      <Section04YourPages />
+
+      {/* SECTION 05 */}
+      <Section05DesignedFromSections />
+
+      {/* SECTION 06 */}
+      <Section06YourBrand />
+
+      {/* SECTION 07 */}
+      <Section07YourControl />
+
+      {/* SECTION 08 */}
+      <Section08EveryInstitution />
+
+      {/* SECTION 09 */}
+      <Section09MakeItYours />
+
+      {/* SECTION 10 */}
+      <Section10CompleteGovernance />
+
+      {/* SECTION 11 */}
+      <Section11EngineeredForScale />
+
+      {/* SECTION 12 */}
+      <Section12OneExperience />
+
+      {/* SECTION 13 */}
+      <Section13BuildWhatComesNext />
+    </main>
+  );
 }
+
 
 
 
